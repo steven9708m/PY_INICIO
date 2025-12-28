fb31d2a13886da571a69c51be49b01b09a5c1624
Steven Miranda <steven08m@outlook.com>
2025-12-27 20:48:07 -0500

    Primeros Pasos

---
+++ 02_CSharp/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace HelloWorldApp
+{                                     
+class HelloWorld
+    {
+    static void Main(string[] args)
+     {
+        //Hola, C#
+        Console.WriteLine("Hello, C#");
+        string myString = "Esto es una cadena de texto.";
+        Console.WriteLine(myString);
+        int myInt = 8;
+        myInt = myInt + 4;
+        Console.WriteLine(myInt);
+        Console.WriteLine(myInt - 2);
+        Console.WriteLine(myInt);
+     }
+    }
+}
+
