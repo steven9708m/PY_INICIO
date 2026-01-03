2060ee59e85703813d44b111885a0c39ea2162bb
Steven Miranda <steven08m@outlook.com>
2026-01-02 20:51:14 -0500

    Aprendizaje

--- 01_Python/00_Hello_World.py
+++ 01_Python/00_Hello_World.py
@@ -1,7 +1,14 @@
+#Nuestro Hola Mundo.
 print("Hola mundo")
 print("¡Bienvenido a Python!")
 print('Hola Mundo con comillas simples')
 
 ''' Este es un comentario en Python '''
 """ Este también es un comentario """
+"""
+Este es un comentario en varias líneas
+que puede abarcar más de una línea.
+"""
 print(type("Hola que tal"))  # Imprime el tipo de dato de la cadena
+print(type(42))              # Imprime el tipo de dato del número entero
+print(type(3.14))            # Imprime el tipo de dato del número flotante
